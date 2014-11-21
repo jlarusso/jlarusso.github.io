04730815f31977b60e28690af62c3f65e05c5608
Jesse La Russo <jlarusso@canvas.is>
2014-11-21 14:21:09 -0500

    Put monkey back in

--- js/hero.js
+++ js/hero.js
@@ -1,14 +1,77 @@
-var heroText = document.getElementById("heroText");
+var likes = [
+  "web-developer",
+  "educator",
+  "cyclist",
+  "painter",
+  "brewer",
+  "geography-nerd",
+  "drawer",
+  "reader",
+  "walker",
+  "traveler",
+  "rubyist",
+  "tinkerer",
+  "hiker",
+  "skiier",
+  "builder",
+  "architect",
+  "film-fan",
+  "javascriptist",
+  "rails-nut",
+  "space-cadet",
+  "science-fan"
+]
 
-var randomWord = function() {
-  return 'a'
+function randomKey() {
+
+  var i = 0
+  var interval = 300
+  var appendWord = function() {
+    if (i < 7) {
+      addWord()
+      i++
+    }
+    setTimeout(appendWord, interval)
+  }
+
+  setTimeout(function(){appendWord()}, 300);
 }
 
-i = 30
-var appendWord = function() {
-  heroText.innerHTML = heroText.innerHTML + randomLetter()
+function addWord() {
+  likes = shuffle(likes)
+  var word = likes.shift()
+  var context = document.getElementById("tagline")
+  context.innerHTML = context.innerHTML + word + ' '
+  context.scrollTop = context.scrollHeight;
 
-  i--
-  if (i > 0) { setTimeout(appendLetter, 100) }
+  if (likes.length < 1) {
+    capslock = document.getElementById("capslock")
+    capslock.removeEventListener("click", addWord);
+    capslock.classList.add('inverted')
+  }
 }
-appendLetter()
+
+// Good ol Fisher-Yates
+function shuffle(array) {
+  var counter = array.length, temp, index;
+
+  // While there are elements in the array
+  while (counter > 0) {
+    // Pick a random index
+    index = Math.floor(Math.random() * counter);
+
+    // Decrease counter by 1
+    counter--;
+
+    // And swap the last element with it
+    temp = array[counter];
+    array[counter] = array[index];
+    array[index] = temp;
+  }
+
+  return array;
+}
+
+
+document.getElementById("capslock").addEventListener("click", addWord);
+randomKey()
